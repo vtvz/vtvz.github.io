c3b1792ac595f3f13db65e68ddc930e6e73fcbb3
Vitaly Zaslavsky <vtvz.ru@gmail.com>
2020-02-06 12:35:42 -0600

    GitLab 115646432

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -103,10 +103,10 @@ $(document).ready(function () {
 /*-----------------------------------------------------------------------------------*/
 /*	PRELOADER
 /*-----------------------------------------------------------------------------------*/
-$(window).load(function () { // makes sure the whole site is loaded
-    $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(350).css({ 'overflow': 'visible' });
+$(window).load(function () {
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+    $('body').delay(350).css({ 'overflow': 'visible' }).addClass('loaded');
 });
 /*-----------------------------------------------------------------------------------*/
 
@@ -133,223 +133,26 @@ window.onload = init();
 /*-----------------------------------------------------------------------------------*/
 /*	SCROLL NAVIGATION HIGHLIGHT
 /*-----------------------------------------------------------------------------------*/
-/*jQuery(document).ready(function () {
-    headerWrapper = parseInt(jQuery('.navbar').height(), 10);
-    var shrinked_header_height = 64,
-        header_height = jQuery('.navbar').height(),
-        navItems = jQuery('.navbar ul a[href^="#"]').not('.navbar ul a[href="#"], .navbar ul a.fancybox-inline');
-    jQuery('.offset').css('padding-top', header_height + 'px');
-    jQuery('.anchor').css('padding-top', shrinked_header_height + 'px');
-    jQuery('.anchor').css('margin-top', -(shrinked_header_height) + 'px');
-    offsetTolerance = -(header_height);
-    //Detecting user's scroll
-    jQuery(window).scroll(function () {
+$(document).ready(function () {
+    var navItems = $('[data-section-id]');
+    var offsetTolerance = 100;
+
+    $(window).scroll(function () {
         //Check scroll position
-        scrollPosition = parseInt(jQuery(this).scrollTop(), 10);
-        //Move trough each menu and check its position with scroll position then add current class
-        navItems.each(function () {
-            var thisHref = jQuery(this).attr('href');
-            if (jQuery(thisHref).length) {
-                thisTruePosition = parseInt(jQuery(thisHref).offset().top, 10);
-                thisPosition = thisTruePosition - headerWrapper - offsetTolerance;
-                if (scrollPosition >= thisPosition) {
-                    jQuery('.current').removeClass('current');
-                    jQuery('.navbar ul a[href=' + thisHref + ']').parent('li').addClass('current');
-                }
+        var scrollPosition = parseInt($(this).scrollTop());
+
+        navItems.each(function (index, item) {
+            var sectionId = $(this).data('section-id');
+            var section = $('#' + sectionId);
+            if (section.length === 0) {
+                return;
+            }
+            var sectionRelativePosition = parseInt(section.offset().top);
+            var sectionPosition = sectionRelativePosition - scrollPosition - offsetTolerance;
+            if (sectionPosition <= 0) {
+                $('.navbar .current').removeClass('current');
+                $('.navbar ul a[data-section-id="' + sectionId + '"]').parent('li').addClass('current');
             }
         });
-        //If we're at the bottom of the page, move pointer to the last section
-        bottomPage = parseInt(jQuery(document).height(), 10) - parseInt(jQuery(window).height(), 10);
-        if (scrollPosition == bottomPage || scrollPosition >= bottomPage) {
-            jQuery('.current').removeClass('current');
-            navItems.last().parent('li').addClass('current');
-        }
     });
-});*/
-/*-----------------------------------------------------------------------------------*/
-/*	CUBE PORTFOLIO
-/*-----------------------------------------------------------------------------------*/
-(function ($, window, document, undefined) {
-    'use strict';
-    var gridContainer = $('#grid-container'),
-        filtersContainer = $('#filters-container'),
-        wrap, filtersCallback;
-    /*********************************
-     init cubeportfolio
-     *********************************/
-    gridContainer.cubeportfolio({
-        layoutMode:       'grid',
-        rewindNav:        true,
-        scrollByPage:     false,
-        defaultFilter:    '*',
-        animationType:    'quicksand',
-        gapHorizontal:    10,
-        gapVertical:      10,
-        gridAdjustment:   'responsive',
-        mediaQueries:     [{
-            width: 1100,
-            cols:  4,
-        }, {
-            width: 800,
-            cols:  3,
-        }, {
-            width: 500,
-            cols:  2,
-        }, {
-            width: 320,
-            cols:  1,
-        }],
-        caption:          'fadeIn',
-        displayType:      'sequentially',
-        displayTypeSpeed: 100,
-
-        // singlePage popup
-        singlePageDelegate:         '.cbp-singlePage',
-        singlePageDeeplinking:      true,
-        singlePageStickyNavigation: true,
-        singlePageCounter:          '',
-        singlePageCallback:         function (url, element) {
-            // to update singlePage content use the following method: this.updateSinglePage(yourContent)
-
-
-            $('a[data-rel]').each(function () {
-                $(this).attr('rel', $(this).data('rel'));
-            });
-
-            var t = this;
-
-            $.ajax({
-                    url:      url,
-                    type:     'GET',
-                    dataType: 'html',
-                    timeout:  5000,
-                })
-                .done(function (result) {
-                    t.updateSinglePage(result);
-                })
-                .fail(function () {
-                    t.updateSinglePage('Error! Please refresh the page!');
-                });
-        },
-    });
-
-
-    /*********************************
-     add listener for filters
-     *********************************/
-    if (filtersContainer.hasClass('cbp-l-filters-dropdown')) {
-        wrap = filtersContainer.find('.cbp-l-filters-dropdownWrap');
-
-        wrap.on({
-            'mouseover.cbp':  function () {
-                wrap.addClass('cbp-l-filters-dropdownWrap-open');
-            },
-            'mouseleave.cbp': function () {
-                wrap.removeClass('cbp-l-filters-dropdownWrap-open');
-            },
-        });
-
-        filtersCallback = function (me) {
-            wrap.find('.cbp-filter-item-custom').removeClass('cbp-filter-item-custom-active');
-            wrap.find('.cbp-l-filters-dropdownHeader').text(me.text());
-            me.addClass('cbp-filter-item-custom-active');
-            wrap.trigger('mouseleave.cbp');
-        };
-    } else {
-        filtersCallback = function (me) {
-            me.addClass('cbp-filter-item-custom-active').siblings().removeClass('cbp-filter-item-custom-active');
-        };
-    }
-
-    filtersContainer.on('click.cbp', '.cbp-filter-item-custom', function () {
-        var me = $(this);
-
-        if (me.hasClass('cbp-filter-item-custom-active')) {
-            return;
-        }
-
-        // get cubeportfolio data and check if is still animating (reposition) the items.
-        if (!$.data(gridContainer[0], 'cubeportfolio').isAnimating) {
-            filtersCallback.call(null, me);
-        }
-
-        // filter the items
-        gridContainer.cubeportfolio('filter', me.data('filter'), function () {
-        });
-    });
-
-
-    /*********************************
-     activate counter for filters
-     *********************************/
-    gridContainer.cubeportfolio('showCounter', filtersContainer.find('.cbp-filter-item-custom'), function () {
-        // read from url and change filter active
-        var match = /#cbpf=(.*?)([#|?&]|$)/gi.exec(location.href),
-            item;
-        if (match !== null) {
-            item = filtersContainer.find('.cbp-filter-item-custom').filter('[data-filter="' + match[1] + '"]');
-            if (item.length) {
-                filtersCallback.call(null, item);
-            }
-        }
-    });
-
-
-    /*********************************
-     add listener for load more
-     *********************************/
-    $('.cbp-l-loadMore-button-link').on('click.cbp', function (e) {
-        e.preventDefault();
-        var clicks, me = $(this),
-            oMsg;
-
-        if (me.hasClass('cbp-l-loadMore-button-stop')) {
-            return;
-        }
-
-        // get the number of times the loadMore link has been clicked
-        clicks = $.data(this, 'numberOfClicks');
-        clicks = (clicks) ? ++clicks : 1;
-        $.data(this, 'numberOfClicks', clicks);
-
-        // set loading status
-        oMsg = me.text();
-        me.text('LOADING...');
-
-        // perform ajax request
-        $.ajax({
-            url:      me.attr('href'),
-            type:     'GET',
-            dataType: 'HTML',
-        }).done(function (result) {
-            var items, itemsNext;
-
-            // find current container
-            items = $(result).filter(function () {
-                return $(this).is('div' + '.cbp-loadMore-block' + clicks);
-            });
-
-            gridContainer.cubeportfolio('appendItems', items.html(),
-                function () {
-                    // put the original message back
-                    me.text(oMsg);
-
-                    // check if we have more works
-                    itemsNext = $(result).filter(function () {
-                        return $(this).is('div' + '.cbp-loadMore-block' + (clicks + 1));
-                    });
-
-                    if (itemsNext.length === 0) {
-                        me.text('NO MORE WORKS');
-                        me.addClass('cbp-l-loadMore-button-stop');
-                    }
-
-                });
-
-        }).fail(function () {
-            // error
-        });
-
-    });
-
-})(jQuery, window, document);
+});
